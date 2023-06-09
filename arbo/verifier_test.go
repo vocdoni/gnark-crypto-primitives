b1996b6ed964b98045c39414014c9c36677b0dca
Lucas Menéndez <hi@lucasmenendez.me>
2023-06-09 09:26:00 +0200

    testing inputs renamed

--- arbo/verifier_test.go
+++ arbo/verifier_test.go
@@ -31,10 +31,10 @@ func TestVerifier(t *testing.T) {
 	assert := test.NewAssert(t)
 
 	var circuit testVerifierCircuit
-	noirTestRoot, _ := new(big.Int).SetString("21135506078746510573119705753579567335835726524098367527812922933644667691006", 10)
-	noirTestKey, _ := new(big.Int).SetString("500400244448261235194511589700085192056257072811", 10)
-	noirTestValue, _ := new(big.Int).SetString("10", 10)
-	noirTestSiblings := []string{
+	testRoot, _ := new(big.Int).SetString("21135506078746510573119705753579567335835726524098367527812922933644667691006", 10)
+	testKey, _ := new(big.Int).SetString("500400244448261235194511589700085192056257072811", 10)
+	testValue, _ := new(big.Int).SetString("10", 10)
+	testSiblings := []string{
 		"13175438946403099127785287940793227584022396513432127658229341995655669945927",
 		"8906855681626013805208515602420790146700990181185755277830603493975762067087",
 		"9457781280074316365191154663065840032069867769247887694941521931147573919101",
@@ -44,23 +44,23 @@ func TestVerifier(t *testing.T) {
 		"572541247728029242828004565014369314635015057986897745288271497923406188177",
 		"9738042754594087795123752255236264962836518315799343893748681096434196901468",
 	}
-	noirTestNSiblings := big.NewInt(int64(len(noirTestSiblings)))
+	testNSiblings := big.NewInt(int64(len(testSiblings)))
 
-	encNoirTestSiblings := [160]frontend.Variable{}
+	enctestSiblings := [160]frontend.Variable{}
 	for i := 0; i < 160; i++ {
-		if i < len(noirTestSiblings) {
-			encNoirTestSiblings[i], _ = new(big.Int).SetString(noirTestSiblings[i], 10)
+		if i < len(testSiblings) {
+			enctestSiblings[i], _ = new(big.Int).SetString(testSiblings[i], 10)
 		} else {
-			encNoirTestSiblings[i] = big.NewInt(0)
+			enctestSiblings[i] = big.NewInt(0)
 		}
 	}
 
 	inputs := testVerifierCircuit{
-		Root:      noirTestRoot,
-		Key:       noirTestKey,
-		Value:     noirTestValue,
-		NSiblings: noirTestNSiblings,
-		Siblings:  encNoirTestSiblings,
+		Root:      testRoot,
+		Key:       testKey,
+		Value:     testValue,
+		NSiblings: testNSiblings,
+		Siblings:  enctestSiblings,
 	}
 	assert.SolvingSucceeded(&circuit, &inputs, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
 }
--- smt/verifier_test.go
+++ smt/verifier_test.go
@@ -25,10 +25,10 @@ func TestVerifier(t *testing.T) {
 	assert := test.NewAssert(t)
 
 	var circuit testVerifierCircuit
-	noirTestRoot, _ := new(big.Int).SetString("21135506078746510573119705753579567335835726524098367527812922933644667691006", 10)
-	noirTestKey, _ := new(big.Int).SetString("500400244448261235194511589700085192056257072811", 10)
-	noirTestValue, _ := new(big.Int).SetString("10", 10)
-	noirTestSiblings := []string{
+	testRoot, _ := new(big.Int).SetString("21135506078746510573119705753579567335835726524098367527812922933644667691006", 10)
+	testKey, _ := new(big.Int).SetString("500400244448261235194511589700085192056257072811", 10)
+	testValue, _ := new(big.Int).SetString("10", 10)
+	testSiblings := []string{
 		"13175438946403099127785287940793227584022396513432127658229341995655669945927",
 		"8906855681626013805208515602420790146700990181185755277830603493975762067087",
 		"9457781280074316365191154663065840032069867769247887694941521931147573919101",
@@ -39,20 +39,20 @@ func TestVerifier(t *testing.T) {
 		"9738042754594087795123752255236264962836518315799343893748681096434196901468",
 	}
 
-	encNoirTestSiblings := [160]frontend.Variable{}
+	enctestSiblings := [160]frontend.Variable{}
 	for i := 0; i < 160; i++ {
-		if i < len(noirTestSiblings) {
-			encNoirTestSiblings[i], _ = new(big.Int).SetString(noirTestSiblings[i], 10)
+		if i < len(testSiblings) {
+			enctestSiblings[i], _ = new(big.Int).SetString(testSiblings[i], 10)
 		} else {
-			encNoirTestSiblings[i] = big.NewInt(0)
+			enctestSiblings[i] = big.NewInt(0)
 		}
 	}
 
 	inputs := testVerifierCircuit{
-		Root:     noirTestRoot,
-		Key:      noirTestKey,
-		Value:    noirTestValue,
-		Siblings: encNoirTestSiblings,
+		Root:     testRoot,
+		Key:      testKey,
+		Value:    testValue,
+		Siblings: enctestSiblings,
 	}
 	assert.SolvingSucceeded(&circuit, &inputs, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
 }
