ee5f3b9add64f590835bca4040cc7a4b742c0c27
Lucas Menéndez <hi@lucasmenendez.me>
2024-11-11 10:42:55 +0100

    test naming

--- internal/twistededwards/twistededwards_test.go
+++ internal/twistededwards/twistededwards_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestXxx(t *testing.T) {
+func TestTE2RTETransform(t *testing.T) {
 	p := new(Point)
 	p.X, _ = new(big.Int).SetString("20284931487578954787250358776722960153090567235942462656834196519767860852891", 10)
 	p.Y, _ = new(big.Int).SetString("21185575020764391300398134415668786804224896114060668011215204645513129497221", 10)
