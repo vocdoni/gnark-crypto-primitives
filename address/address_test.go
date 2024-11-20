3cdd9b0913cd89c89982523f33151a6fe7d9a23b
Lucas Menéndez <hi@lucasmenendez.me>
2024-11-20 12:30:33 +0100

    use quicktest and go-ethereum crypto package

--- address/address_test.go
+++ address/address_test.go
@@ -1,7 +1,6 @@
 package address
 
 import (
-	"crypto/rand"
 	"fmt"
 	"math/big"
 	"testing"
@@ -16,7 +15,8 @@ import (
 	"github.com/consensys/gnark/std/math/emulated"
 	gecdsa "github.com/consensys/gnark/std/signature/ecdsa"
 	"github.com/consensys/gnark/test"
-	"github.com/iden3/go-iden3-crypto/keccak256"
+	"github.com/ethereum/go-ethereum/crypto"
+	qt "github.com/frankban/quicktest"
 )
 
 type testAddressCircuit struct {
@@ -34,38 +34,28 @@ func (c *testAddressCircuit) Define(api frontend.API) error {
 }
 
 func TestAddressDerivation(t *testing.T) {
-	input := []byte("test")
+	c := qt.New(t)
+
+	input := crypto.Keccak256Hash([]byte("hello")).Bytes()
 	// generate ecdsa key pair (privKey and publicKey)
-	privKey, err := ecdsa.GenerateKey(rand.Reader)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pubBytes := []byte{}
-	for _, b := range privKey.PublicKey.A.X.Bytes() {
-		pubBytes = append(pubBytes, b)
-	}
-	for _, b := range privKey.PublicKey.A.Y.Bytes() {
-		pubBytes = append(pubBytes, b)
-	}
-	hash := keccak256.Hash(pubBytes)
-	// compute the signature of an arbitrary message
-	sigBin, err := privKey.Sign(input, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if flag, err := privKey.PublicKey.Verify(sigBin, input, nil); !flag || err != nil {
-		t.Fatal("invalid signature")
-	}
+	privKey, err := crypto.GenerateKey()
+	c.Assert(err, qt.IsNil)
+	sigBin, err := crypto.Sign(input, privKey)
+	c.Assert(err, qt.IsNil)
+	// truncate the signature to 64 bytes (the first 32 bytes are the R value,
+	// the second 32 bytes are the S value)
+	sigBin = sigBin[:64]
+	valid := crypto.VerifySignature(crypto.CompressPubkey(&privKey.PublicKey), input, sigBin)
+	c.Assert(valid, qt.IsTrue)
 	var sig ecdsa.Signature
-	if _, err := sig.SetBytes(sigBin); err != nil {
-		t.Fatal(err)
-	}
+	_, err = sig.SetBytes(sigBin)
+	c.Assert(err, qt.IsNil)
 	r, s := new(big.Int), new(big.Int)
 	r.SetBytes(sig.R[:32])
 	s.SetBytes(sig.S[:32])
 	// get the address from the hash of the public key (taking the last 20 bytes
 	// of the Keccak-256 hash of the public key)
-	address := new(big.Int).SetBytes(hash[12:])
+	address := crypto.PubkeyToAddress(privKey.PublicKey)
 	// compile the circuit and get the constraints
 	p := profile.Start()
 	now := time.Now()
@@ -75,10 +65,10 @@ func TestAddressDerivation(t *testing.T) {
 	fmt.Println("constrains", p.NbConstraints())
 	// init inputs
 	witness := testAddressCircuit{
-		Address: address,
+		Address: address.Big(),
 		PublicKey: gecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
-			X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.X),
-			Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.A.Y),
+			X: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.X),
+			Y: emulated.ValueOf[emulated.Secp256k1Fp](privKey.PublicKey.Y),
 		},
 	}
 	assert := test.NewAssert(t)
