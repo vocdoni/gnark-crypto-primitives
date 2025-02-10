295ac4089fd83535d006a7dfcd88278d6eac3bd0
Lucas Menéndez <hi@lucasmenendez.me>
2025-02-10 16:30:45 +0100

    new FromEmulatedTEtoRTE and FromEmulatedRTEtoTE primitives to convert points using emulated elements form babyjubjub between twisted edwards and reduced twisted edwards formats

--- emulated/bn254/twistededwards/twistededwards.go
+++ emulated/bn254/twistededwards/twistededwards.go
@@ -7,9 +7,16 @@
 // Read more about this here: https://github.com/bellesmarta/baby_jubjub
 package twistededwards
 
-import "github.com/consensys/gnark/frontend"
+import (
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/emulated"
+)
 
 var scalingFactor = frontend.Variable("6360561867910373094066688120553762416144456282423235903351243436111059670888")
+var emulatedScalingFactor = emulated.Element[sw_bn254.ScalarField]{
+	Limbs: []frontend.Variable{7817090900423792488, 8405395627841593623, 3205086078052995447, 1013295966202553675},
+}
 
 func FromRTEtoTE(api frontend.API, x, y frontend.Variable) (frontend.Variable, frontend.Variable) {
 	// compute negF = -f mod p
@@ -27,4 +34,30 @@ func FromTEtoRTE(api frontend.API, x, y frontend.Variable) (frontend.Variable, f
 	// compute xRTE = x * (-f)
 	xRTE := api.Mul(x, negF)
 	return xRTE, y
+}
+
+func FromEmulatedRTEtoTE(api frontend.API, x, y emulated.Element[sw_bn254.ScalarField]) (emulated.Element[sw_bn254.ScalarField], emulated.Element[sw_bn254.ScalarField], error) {
+	field, err := emulated.NewField[sw_bn254.ScalarField](api)
+	if err != nil {
+		return emulated.Element[sw_bn254.ScalarField]{}, emulated.Element[sw_bn254.ScalarField]{}, err
+	}
+	// compute negF = -f mod p
+	negF := field.Neg(&emulatedScalingFactor)
+	// compute the inverse of negF in the field
+	negFInv := field.Inverse(negF)
+	// compute xTE = x / (-f)
+	xTE := field.Mul(&x, negFInv)
+	return *xTE, y, nil
+}
+
+func FromEmulatedTEtoRTE(api frontend.API, x, y emulated.Element[sw_bn254.ScalarField]) (emulated.Element[sw_bn254.ScalarField], emulated.Element[sw_bn254.ScalarField], error) {
+	field, err := emulated.NewField[sw_bn254.ScalarField](api)
+	if err != nil {
+		return emulated.Element[sw_bn254.ScalarField]{}, emulated.Element[sw_bn254.ScalarField]{}, err
+	}
+	// compute negF = -f mod p
+	negF := field.Neg(&emulatedScalingFactor)
+	// compute xRTE = x * (-f)
+	xRTE := field.Mul(&x, negF)
+	return *xRTE, y, nil
 }
--- emulated/bn254/twistededwards/twistededwards_test.go
+++ emulated/bn254/twistededwards/twistededwards_test.go
@@ -11,37 +11,71 @@ import (
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
 	"github.com/consensys/gnark/profile"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/test"
 	"github.com/vocdoni/vocdoni-z-sandbox/crypto/ecc/format"
 )
 
 type testFromTwistedEdwards struct {
 	X, Y, XPrime, YPrime frontend.Variable
+
+	EX, EY, EXPrime, EYPrime emulated.Element[sw_bn254.ScalarField]
 }
 
 func (c *testFromTwistedEdwards) Define(api frontend.API) error {
 	xPrime, yPrime := FromTEtoRTE(api, c.X, c.Y)
 	api.AssertIsEqual(xPrime, c.XPrime)
 	api.AssertIsEqual(yPrime, c.YPrime)
+
+	EXPrime, EYPrime, err := FromEmulatedTEtoRTE(api, c.EX, c.EY)
+	if err != nil {
+		return err
+	}
+
+	field, err := emulated.NewField[sw_bn254.ScalarField](api)
+	if err != nil {
+		return err
+	}
+	field.AssertIsEqual(&EXPrime, &c.EXPrime)
+	field.AssertIsEqual(&EYPrime, &c.EYPrime)
 	return nil
 }
 
 type testToTwistedEdwards struct {
 	X, Y, XPrime, YPrime frontend.Variable
+
+	EX, EY, EXPrime, EYPrime emulated.Element[sw_bn254.ScalarField]
 }
 
 func (c *testToTwistedEdwards) Define(api frontend.API) error {
 	xPrime, yPrime := FromRTEtoTE(api, c.X, c.Y)
 	api.AssertIsEqual(xPrime, c.XPrime)
 	api.AssertIsEqual(yPrime, c.YPrime)
+
+	EXPrime, EYPrime, err := FromEmulatedRTEtoTE(api, c.EX, c.EY)
+	if err != nil {
+		return err
+	}
+
+	field, err := emulated.NewField[sw_bn254.ScalarField](api)
+	if err != nil {
+		return err
+	}
+	field.AssertIsEqual(&EXPrime, &c.EXPrime)
+	field.AssertIsEqual(&EYPrime, &c.EYPrime)
 	return nil
 }
 
 func TestFromTwistedEdwards(t *testing.T) {
 	x, _ := new(big.Int).SetString("20284931487578954787250358776722960153090567235942462656834196519767860852891", 10)
+	eX := emulated.ValueOf[sw_bn254.ScalarField](x)
 	y, _ := new(big.Int).SetString("21185575020764391300398134415668786804224896114060668011215204645513129497221", 10)
+	eY := emulated.ValueOf[sw_bn254.ScalarField](y)
 
 	xRTE, yRTE := format.FromTEtoRTE(x, y)
+	eXRTE := emulated.ValueOf[sw_bn254.ScalarField](xRTE)
+	eYRTE := emulated.ValueOf[sw_bn254.ScalarField](yRTE)
 	// profiling the circuit compilation
 	p := profile.Start()
 	now := time.Now()
@@ -53,10 +87,14 @@ func TestFromTwistedEdwards(t *testing.T) {
 	// run the test circuit
 	assert := test.NewAssert(t)
 	inputs := &testFromTwistedEdwards{
-		X:      x,
-		Y:      y,
-		XPrime: xRTE,
-		YPrime: yRTE,
+		X:       x,
+		Y:       y,
+		XPrime:  xRTE,
+		YPrime:  yRTE,
+		EX:      eX,
+		EY:      eY,
+		EXPrime: eXRTE,
+		EYPrime: eYRTE,
 	}
 	now = time.Now()
 	assert.SolvingSucceeded(&testFromTwistedEdwards{}, inputs, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
@@ -65,9 +103,13 @@ func TestFromTwistedEdwards(t *testing.T) {
 
 func TestFromReducedTwistedEdwards(t *testing.T) {
 	x, _ := new(big.Int).SetString("20284931487578954787250358776722960153090567235942462656834196519767860852891", 10)
+	ex := emulated.ValueOf[sw_bn254.ScalarField](x)
 	y, _ := new(big.Int).SetString("21185575020764391300398134415668786804224896114060668011215204645513129497221", 10)
+	ey := emulated.ValueOf[sw_bn254.ScalarField](y)
 
 	xRTE, yRTE := format.FromTEtoRTE(x, y)
+	exRTE := emulated.ValueOf[sw_bn254.ScalarField](xRTE)
+	eyRTE := emulated.ValueOf[sw_bn254.ScalarField](yRTE)
 	// profiling the circuit compilation
 	p := profile.Start()
 	now := time.Now()
@@ -79,10 +121,14 @@ func TestFromReducedTwistedEdwards(t *testing.T) {
 	// run the test circuit
 	assert := test.NewAssert(t)
 	inputs := &testToTwistedEdwards{
-		X:      xRTE,
-		Y:      yRTE,
-		XPrime: x,
-		YPrime: y,
+		X:       xRTE,
+		Y:       yRTE,
+		XPrime:  x,
+		YPrime:  y,
+		EX:      exRTE,
+		EY:      eyRTE,
+		EXPrime: ex,
+		EYPrime: ey,
 	}
 	assert.SolvingSucceeded(&testToTwistedEdwards{}, inputs, test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
 }
