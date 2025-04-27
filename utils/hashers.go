98a84de4872a62267aada9807f30f259d980ba40
p4u <pau@dabax.net>
2025-04-27 21:25:33 +0200

    hash: add poseidon2
    Signed-off-by: p4u <pau@dabax.net>

--- utils/hashers.go
+++ utils/hashers.go
@@ -2,7 +2,12 @@ package utils
 
 import (
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/hash"
 	"github.com/consensys/gnark/std/hash/mimc"
+
+	//"github.com/consensys/gnark/std/hash/poseidon2"
+	poseidon2bn254 "github.com/consensys/gnark-crypto/ecc/bn254/fr/poseidon2"
+	"github.com/consensys/gnark/std/permutation/poseidon2"
 )
 
 type Hasher func(frontend.API, ...frontend.Variable) (frontend.Variable, error)
@@ -15,6 +20,24 @@ func MiMCHasher(api frontend.API, data ...frontend.Variable) (frontend.Variable,
 	if err != nil {
 		return 0, err
 	}
+	h.Write(data...)
+	return h.Sum(), nil
+}
+
+// Poseidon2Hasher is a hash function that hashes the data provided using the
+// poseidon2 hash function and the current compiler field.
+func Poseidon2Hasher(api frontend.API, data ...frontend.Variable) (frontend.Variable, error) {
+	params := poseidon2bn254.GetDefaultParameters()
+	f, err := poseidon2.NewPoseidon2FromParameters(
+		api,
+		params.Width,
+		params.NbFullRounds,
+		params.NbPartialRounds,
+	)
+	if err != nil {
+		return 0, err
+	}
+	h := hash.NewMerkleDamgardHasher(api, f, 0)
 	h.Write(data...)
 	return h.Sum(), nil
 }
